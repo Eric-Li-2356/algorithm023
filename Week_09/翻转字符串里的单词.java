d690fb15466f1843958078fe40549cd11513a951
Eric-Li-2356 <yanghui.li.st@gmail.com>
2021-03-07 23:46:10 +0800

    week09 assignment

---
+++ Week_09/翻转字符串里的单词.java
@@ -0,0 +1,43 @@
+/**
+* API法
+*/
+class Solution {
+    public String reverseWords(String s) {
+        s = s.trim();
+        List<String> wordList = Arrays.asList(s.split("\\s+"));
+        Collections.reverse(wordList);
+
+        return String.join(" ", wordList);
+    }
+}
+
+/**
+* 双端队列法
+*/
+class Solution {
+    public String reverseWords(String s) {
+        int left = 0, right = s.length() - 1;
+        while (left <= right && s.charAt(left) == ' ') {
+            ++left;
+        }
+        while (left <= right && s.charAt(right) == ' ') {
+            --right;
+        }
+
+        Deque<String> d = new ArrayDeque<>();
+        StringBuilder word = new StringBuilder();
+        while (left <= right) {
+            char c = s.charAt(left);
+            if ((word.length() != 0) && (c == ' ')) {
+                d.offerFirst(word.toString());
+                word.setLength(0);
+            } else if (c != ' ') {
+                word.append(c);
+            }
+            ++left;
+        }
+        d.offerFirst(word.toString());
+
+        return String.join(" ", d);
+    }
+}
